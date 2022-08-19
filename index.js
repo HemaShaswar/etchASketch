09461ad65ab7d2ca8eb1dffc53df74d8d9002a11
hema <shaswarhema@gmail.com>
2022-08-19 20:49:20 +0300

    setting up the files and linking them

---
+++ index.js
@@ -0,0 +1,7 @@
+const board = document.querySelector(".board");
+const square = document.createElement("div");
+square.classList.add("square");
+square.textContent = "                 "
+board.appendChild(square);
+
+
